860431a79dd2644108c3cdb4b5aa9b249558de80
Clement Macharia <clementmacharia62@gmail.com>
2024-11-06 16:06:16 +0300

    updated the serializer section

--- klinsept/app/serializers.py
+++ klinsept/app/serializers.py
@@ -1,21 +1,25 @@
 from rest_framework import serializers
-from .models import User,Category,Product,Order,Review,Payment,ShippingAddress
+from .models import User, Category, Product, Order, Review, Payment, ShippingAddress
 
 # to create serialized data for json 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['id', 'first_name', 'last_name', 'email', 'phone_number', 'location', 'created_at']
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ['id','name','description']
+        fields = ['id', 'name']
 
 class ProductSerializer(serializers.ModelSerializer):
-    category = CategorySerializer(read_only = True)
-    
+    # We will use a SerializerMethodField to transform the category
+    category = serializers.SerializerMethodField()
+
     class Meta:
         model = Product
-        fields = ['id','name','description','image','price','stock','category','created_at','updated_at']
+        fields = ['id', 'name', 'description', 'image', 'price', 'stock', 'category', 'created_at', 'updated_at']
+
+    def get_category(self, obj):
+        return [obj.category.name]  
