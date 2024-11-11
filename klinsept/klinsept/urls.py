5c100015a86924774f31c4a2b15d68a8ddfe5571
Clement Macharia <clementmacharia62@gmail.com>
2024-11-11 17:17:19 +0300

    updated the routes for register and login added cookies

--- klinsept/klinsept/urls.py
+++ klinsept/klinsept/urls.py
@@ -16,21 +16,37 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app.views import getProducts,signUp,Login,password_reset_otp,verify_otp,Logout
+from app.views import getProducts,RegisterUser,LoginUser,password_reset_otp,verify_otp,Logout,get_product_by_id,contact,add_to_cart,get_cart_items,remove_from_cart,create_order,test_authentication,get_cookie
 
 urlpatterns = [
     path('admin/', admin.site.urls), #to be changed and added api/v1.0 to be use with api keys
+    # get all products
     path('api/v1.0/products/', getProducts, name='get_products'),
-    # signup user
-    path('api/v1.0/auth/signin/',signUp,name='signUp_user'),
+    # Get product by id
+    path('api/v1.0/product/<int:id>',get_product_by_id,name="get_indivual_product"),    # signup user
+    # register user
+    path('api/v1.0/auth/signin/',RegisterUser,name='signUp_user'),
     # login 
-    path('api/v1.0/auth/login/',Login,name='login_user'),
+    path('api/v1.0/auth/login/',LoginUser,name='login_user'),
+    # get user cookie
+    path('api/v1.0/auth/cookie/',get_cookie,name='login_user'),
     # get otp 
     path('api/v1.0/auth/otp/request/',password_reset_otp,name="password_reset_otp"),
     # Reset password and confirm otp
     path('api/v1.0/auth/otp/confirm/',verify_otp, name = "otp_confirmation"),
     # Logout user
-    path('api/v1.0/auth/logout/',Logout, name = "otp_confirmation")
-
+    path('api/v1.0/auth/logout/',Logout, name = "otp_confirmation"),
+    # Contact client
+    path('api/v1.0/contact',contact,name='contact company'),
+    # add items to cart
+    path('api/v1.0/cart/add/',add_to_cart, name='add_to_cart'),
+    # get items in cart
+    path('api/v1.0/cart/', get_cart_items, name='get_cart_items'),
+    # remove items from cart
+    path('api/v1.0/cart/remove/<int:id>', remove_from_cart, name='remove_from_cart'),
+    # place order
+    path('api/v1.0/order/', create_order, name='create_order'),
+    # test 
+    path('api/v1.0/test/',test_authentication, name='get_products'),
 
 ]
